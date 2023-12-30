e9bf9421a484c51ce4768d0fc6d40f5272a87a77
Tryd0g0lik <work80@mail.ru>
2023-12-30 18:29:21 +0700

    [03.1]: The code 'TrainFC' function corrected. Location 'TrainFC' function was changed. And was created  content the auto update

--- src/frontend/components/Counter/Counter.tsx
+++ src/frontend/components/Counter/Counter.tsx
@@ -3,21 +3,54 @@ import InputFC from "./Inputs.tsx";
 import ButtonFC from "../Buttons.tsx";
 const pageLoder = require("../../steps/pageLoder/index.ts");
 import TrainFC from "../Training/Train.tsx";
-
-export default function CounterFC(): React.JSX.Element {
+// interface ElementProps {
+//   setRecords: (records: any) => void;
+// }
+// const
+let loacalStor = '{}';
+if (typeof localStorage.getItem('heandlersData') === 'string') {
+  loacalStor = localStorage.getItem('heandlersData') as string;
+}
+export default function CounterFC() {
   const uniqueInputId = useId();
-  const { } = useState()
-
-  const handlePress = (event: React.FormEvent<HTMLFormElement>): void => {
+  const loacalStorJSON = JSON.parse(loacalStor);
+  const [records, setRecords] = useState<JSX.Element>(<TrainFC prop={loacalStorJSON} />);
+  // < TrainFC />
+  console.log(`[dataLStoorageGet] records: ${records}`);
+  const handlePress = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = new FormData(event.target as HTMLFormElement);
     let date = formData.get("date") as string;
     let distance = formData.get("distanc") as string;
-    pageLoder({ 'insert': [{ 'date': date.slice(0), 'distance': distance.slice(0) }] }); // { 'date': date.slice(0) }, { 'distance': distance.slice(0) }
+    pageLoder({ 'insert': [{ 'date': date.slice(0), 'distance': distance.slice(0) }] }); 
     event.currentTarget.reset();
-
+    return newRecordAdd();
   };
+  function newRecordAdd() {
+    let status = localStorage.getItem('heandlersData')
+    if (typeof status === 'string') {
+      // let status = status as string;
+      // console.log(`[CounterFC localStorage]: ${status}, ${typeof status}`);
+      const bool = loacalStor.indexOf(status) === -1 ? true : false;
+      console.log(`[dataLStoorageGet] BOOL: ${bool}`)
+
+      if (bool) {
+        // const statusJSON = status;
+        loacalStor = status;
+        return setRecords(<TrainFC prop={status} />);
+
+      } else {
+        setTimeout(() => {
+          newRecordAdd()
+          console.log(`[dataLStoorageGet]: setTimeout`)
+        }, 1200)
+      }
+
+    }
+    return loacalStor
+  }
+
   return (
     <>
       <form className="form" onSubmit={handlePress as React.FormEventHandler<HTMLFormElement>} >
@@ -37,8 +70,8 @@ export default function CounterFC(): React.JSX.Element {
         </div>
 
       </div>
-    </form>
-      <TrainFC />
+      </form>
+      {records}
     </>
   );
 }
--- src/frontend/components/Training/Train.tsx
+++ src/frontend/components/Training/Train.tsx
@@ -3,23 +3,11 @@ import HandleRemoveFC from "./HandleRemove.tsx";
 const Pulisher = require("../../steps/piblisher-data/index.ts");
 const publisher = new Pulisher()
 import ListRecords from "../Records/index.tsx";
-
-export default function TrainFC(): React.JSX.Element {
-  let trainigs: any[] = [];
-  let result: (string | Record<string, Record<string, string>>) = '';
+// let result: (string | Record<string, Record<string, string>>) = '';
+export default function TrainFC(prop: any): React.JSX.Element {
+// let trainigs: any[] = [];
+// let result: (string | Record<string, Record<string, string>>) = prop.status;
   const resp = publisher.dataGetForPublish;
-  console.log(`[dataLStoorageGet]: publisher.publish: ${(resp)}`);
-
-  result = resp === false ? (
-    // setTimeout(() => {
-    //   dataLStoorageGet(timeout);
-    //   console.log(`[dataLStoorageGet]: setTimeout`)
-    // }, timeout),
-    '{}'
-  ) : (
-    resp
-  );
-  if (typeof result !== "object") {
     return (
       <div className="feature">
         <ul className="head">
@@ -33,14 +21,6 @@ export default function TrainFC(): React.JSX.Element {
         </div>
       </div>
     );
-  }
-  return (
-    < ul className="content"  >
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul >
-  )
+
 
 }
