86c8844bd0857e8128b042fa9ae0d81d31b1e86d
Tryd0g0lik <work80@mail.ru>
2024-01-13 12:25:13 +0700

    [5]: the sorter code has been little cleaned up

--- src/frontend/steps/sorter/index.ts
+++ src/frontend/steps/sorter/index.ts
@@ -1,23 +1,20 @@
 // src\frontend\steps\sorter\index.ts
 
-// type Sort = Record<string, Record<string, string>>;
+type Sort = Record<string, Record<string, string>>;
 
-export default function sorter(obj: Record<string, Record<string, string>>): Record<string, Record<string, string>> {
-  const sorting: Record<any, any> | Record<string, Record<string, string>> = {};
-  // const map = new Map();
+export default function sorter(obj: Sort): Sort {
+  const sorting: Record<any, any> | Sort = {};
   const arr: any[] = [];
-  // let ind: number = 0;
   const listKeys = Object.keys(obj);
-  // const listValues = Object.values(obj);
   console.log("[SORT]: Keys: ", listKeys);
   listKeys.forEach((key: string) => {
-    const line: Record<string, Record<string, string>> | Record<any, any> = {};
+    const line: Sort | Record<any, any> = {};
     line[key] = obj[key];
     console.log("[SORT]: cycle: ", key);
     arr.push(line);
   });
 
-  arr.sort((a: Record<string, Record<string, string>>, b: Record<string, Record<string, string>>) => {
+  arr.sort((a: Sort, b: Sort) => {
     console.log("[SORT]: sort: ", a);
     const key1 = Object.keys(a)[0];
     const key2 = Object.keys(b)[0];
@@ -31,10 +28,11 @@ export default function sorter(obj: Record<string, Record<string, string>>): Rec
 
   console.log("[SORT]: ARR: ", arr);
 
-  arr.forEach((item: Record<string, Record<string, string>>) => {
+  arr.forEach((item: Sort) => {
     const key: string[] = Object.keys(item);
     sorting[key[0]] = item[key[0]];
   });
+
   console.log("[SORT]: DATE: ", Object.keys(sorting));
   return sorting;
 }
